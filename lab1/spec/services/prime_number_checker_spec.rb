8a9b8b1b23df5db5e0b9bb6a6dd8673c60da49c4
Hubert Ko <hk190696@gmail.com>
2021-12-05 12:08:54 +0100

    przeniesienie reszty

---
+++ lab1/spec/services/prime_number_checker_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe PrimeNumberChecker do
+  let(:numbers) { (3..11) }
+  subject { PrimeNumberChecker.new(numbers) }
+  describe '#last_prime?' do
+    it 'should return true for prime number' do
+      expect(subject.last_prime?).to be_truthy
+    end
+
+    it 'should return false for no prime number' do
+      numbers = (1..10)
+      pnc     = PrimeNumberChecker.new(numbers)
+      expect(pnc.last_prime?).to be_falsey
+    end
+  end
+
+  describe '#first_prime?' do
+    context 'when first is prime' do
+      it { expect(subject.first_prime?).to be_truthy }
+    end
+
+    context 'when first is not prime' do
+      let(:numbers) { (4..11) }
+      it { expect(subject.first_prime?).to be_falsey }
+    end
+
+    context 'when array is empty' do
+      let(:numbers) { [] }
+      it { expect(subject.first_prime?).to be_nil }
+    end
+  end
+
+  describe '#prime_filter' do
+    context 'when array has prime numbers' do
+      let(:numbers) { (1..11) }
+      it { expect(subject.prime_filter).to_not be_empty }
+      it { expect(subject.prime_filter).to eq([2, 3, 5, 7, 11]) }
+    end
+
+    context 'when array dose not has prime numbers' do
+      let(:numbers) { [4, 6, 8, 9] }
+      it { expect(subject.prime_filter).to be_empty }
+      it { expect(subject.prime_filter).to_not be_nil }
+    end
+  end
+
+  describe 'no_prime?' do
+    context 'when array dose not prime numbers' do
+      let(:numbers) { [4, 6, 8, 9] }
+      it { expect(subject.no_prime?).to be_truthy }
+    end
+
+    context 'when array has prime numbers' do
+      it { expect(subject.no_prime?).to be_falsey }
+    end
+  end
+end
